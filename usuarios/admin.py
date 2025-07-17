aeca622b69119227331c3969db1803ea4841e394
sofigonzalezz1 <sa.gonzalez@itecriocuarto.org.ar>
2025-07-17 20:45:34 -0300

    creacion y migracion de modelo usuario

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+# usuarios/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import PerfilUsuario
+
+
+@admin.register(PerfilUsuario)
+class PerfilUsuarioAdmin(admin.ModelAdmin):
+    list_display = ('user', 'rol')
+    list_filter = ('rol',)
+    search_fields = ('user__username', 'user__email')
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+# usuarios/models.py
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class PerfilUsuario(models.Model):
+    ROLES = [
+        ('admin', 'Administrador'),
+        ('operador', 'Operador'),
+        ('pasajero', 'Pasajero'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rol = models.CharField(max_length=20, choices=ROLES)
+
+    def __str__(self):
+        return f"{self.user.username} ({self.get_rol_display()})"
