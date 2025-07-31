beda752fcaabd13d7268d330af7fb65bb9427dd8
Imanol72 <imanol_reartes@hotmail.com>
2025-07-31 20:17:57 -0300

    templates pasajeros list y detail

--- trabajo_aerolineas/urls.py
+++ trabajo_aerolineas/urls.py
@@ -6,7 +6,11 @@ urlpatterns = [
     path('vuelos/', include('vuelos.urls')),
     path('pasajeros/', include('pasajeros.urls')),
     path('reservas/', include('reservas.urls')),
+<<<<<<< HEAD
     path('', include('home.urls')),
 
+=======
+    path('pasajeros/', include('pasajeros.urls')),
+>>>>>>> 3332a1e (templates pasajeros list y detail)
 
 ]
