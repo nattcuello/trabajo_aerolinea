e1af24067f5a1c6fd50db94ea6c768bd32df780d
Imanol72 <imanol_reartes@hotmail.com>
2025-07-31 19:08:23 -0300

    borrar templates/base.html borrar templates/home.html modificar trabajo_aerolineas/__pycache__/urls.cpython-312.pyc modificar trabajo_aerolineas/urls.py

--- trabajo_aerolineas/urls.py
+++ trabajo_aerolineas/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('vuelos/', include('vuelos.urls')),  # esta línea es clave
     path('pasajeros/', include('pasajeros.urls')),
+    path('reservas/', include('reservas.urls')),
 
 ]
