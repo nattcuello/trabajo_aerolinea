942a5e550e3fbf4f48992917c0c00ae86d182957
Imanol72 <imanol_reartes@hotmail.com>
2025-07-31 21:03:32 -0300

    Creacion de vista de pasajeros junto con su template y url y detalle de pasajeros

--- pasajeros/urls.py
+++ pasajeros/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lista_pasajeros, name='lista_pasajeros'),
+    path('', views.lista_pasajeros, name='listado_pasajeros'),
     path('<int:pasajero_id>/', views.detalle_pasajero, name='detalle_pasajero'),
 ]
